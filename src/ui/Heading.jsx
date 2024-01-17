e0cfde9c49c1eb7423649a7b8858d53921460696
tysoab <tysoab@gmail.com>
2024-01-17 14:21:35 -0800

    adjust Heading.css

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,4 +1,4 @@
-import style from "./heading.module.css";
+import style from "./Heading.module.css";
 export default function Heading({ children }) {
   return <h1 className={style.heading}>{children}</h1>;
 }
